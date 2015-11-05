598137c0ab00a6d856ff217a05bb0defea9ce56e
JustinGamer <MCAnonymousModders@gmail.com>
2015-11-05 18:29:48 -0500

    Update TC-PE-V1-0.js

--- TC-PE-V1-0.js
+++ TC-PE-V1-0.js
@@ -1,11 +1,19 @@
 /*This mod has been ported by MCElectricGamer. There is another Tinkers Construct ported to MC:PE by MinecraftMuse*/
 
 var TCPE = {};
+TCPE.texture = {};
 TCPE.item = {};
+TCPE.tile = {};
+
+//Wood
+TCPE.texture.wood = {};
+TCPE.texture.wood.rod = "rod_wood";
+TCPE.texture.wood.chunk = "chunk_wood";
 
 //Wood
 TCPE.item.wood = {};
 TCPE.item.wood.rod = 500;
+TCPE.item.wood.chunk = 501;
 
 //Stone
 TCPE.item.stone = {};
@@ -21,6 +29,8 @@ TCPE.useOn = function(x,y,z,i,b,s,id,bd){
 }
 TCPE.tick = function(){
 }
+
+TCPE.setItem(TCPE.item.wood.rod,"Wooden Rod",TCPE.texture.wood.rod,0,64);
 
 //ModPE Hooks
 function useItem(x,y,z,i,b,s,id,bd){TCPE.useOn(x,y,z,i,b,s,id,bd);}
