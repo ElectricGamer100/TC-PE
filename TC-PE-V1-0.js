b605162e2214606e8db58ee9b278dae08762ac7d
JustinGamer <MCAnonymousModders@gmail.com>
2015-11-05 17:59:15 -0500

    Create TC-PE-V1-0.js

---
+++ TC-PE-V1-0.js
@@ -0,0 +1,20 @@
+/*This mod has been ported by MCElectricGamer. There is another Tinkers Construct ported to MC:PE by MinecraftMuse*/
+
+var TCPE = {};
+TCPE.item = {};
+
+//Wood
+TCPE.item.wood = {};
+TCPE.item.wood.rod = 500;
+
+//Stone
+TCPE.item.stone = {};
+
+//Flint
+TCPE.item.flint = {};
+
+//Iron
+TCPE.item.iron = {};
+
+TCPE.useItem = function(x,y,z,i,b,s,id,bd){
+}
